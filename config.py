4410a9363247cd80fccc0f719d1571865a9deb28
777den7771 <777den1996@gmail.com>
2025-05-21 12:56:37 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+TELEGRAM_TOKEN = 'ВАШ_ТОКЕН_ОТ_BOTFATHER'
+CHAT_ID = 'ВАШ_CHAT_ID'
+KIJIJI_SEARCH_URL = 'https://www.kijiji.ca/b-cell-phone/gta-greater-toronto-area/iphone/k0c760l1700272'
+CHECK_INTERVAL = 300
