8aa1bd8b15a64232822dd9e3eac1715f9b82e6e8
777den7771 <777den1996@gmail.com>
2025-05-21 13:19:23 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TELEGRAM_TOKEN = 'ВАШ_ТОКЕН_ОТ_BOTFATHER'
-CHAT_ID = 'ВАШ_CHAT_ID'
+TELEGRAM_TOKEN = '7764776672:AAG77dSGvj9GQAQ65qTeablUymI3M0NdWPE'
+CHAT_ID = '6086012346'
 KIJIJI_SEARCH_URL = 'https://www.kijiji.ca/b-cell-phone/gta-greater-toronto-area/iphone/k0c760l1700272'
 CHECK_INTERVAL = 300
