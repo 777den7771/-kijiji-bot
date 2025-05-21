4db57953569e364458786e85ae205135b7d422d8
777den7771 <777den1996@gmail.com>
2025-05-21 17:58:03 -0500

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ KIJIJI_SEARCH_URL = 'https://www.kijiji.ca/b-cell-phone/gta-greater-toronto-area
 KIJIJI_SEARCH_URL = 'https://https://www.kijiji.ca/b-winnipeg/bike/k0l1700192?address=Winnipeg%2C%20MB&dc=true&ll=49.8954221%2C-97.1385145&radius=16.0&view=list'
 KIJIJI_SEARCH_URL = 'https://www.kijiji.ca/b-winnipeg/laptop/k0l1700192?address=Winnipeg%2C%20MB&dc=true&ll=49.8954221%2C-97.1385145&radius=16.0&view=list'
 
-CHECK_INTERVAL = 300
+CHECK_INTERVAL = 180
