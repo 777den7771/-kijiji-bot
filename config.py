2f40396dc8c12b9bd8cc9ae6d61475163dcc2224
777den7771 <777den1996@gmail.com>
2025-05-21 13:28:39 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 TELEGRAM_TOKEN = '7764776672:AAG77dSGvj9GQAQ65qTeablUymI3M0NdWPE'
 CHAT_ID = '6086012346'
 KIJIJI_SEARCH_URL = 'https://www.kijiji.ca/b-cell-phone/gta-greater-toronto-area/iphone/k0c760l1700272'
-KIJIJI_SEARCH_URL = 'https://https://www.kijiji.ca/b-winnipeg/bike/k0l1700192?address=Winnipeg%2C%20MB&dc=true&ll=49.8954221%2C-97.1385145&radius=16.0&view=list
-KIJIJI_SEARCH_URL = 'https://www.kijiji.ca/b-winnipeg/laptop/k0l1700192?address=Winnipeg%2C%20MB&dc=true&ll=49.8954221%2C-97.1385145&radius=16.0&view=list
+KIJIJI_SEARCH_URL = 'https://https://www.kijiji.ca/b-winnipeg/bike/k0l1700192?address=Winnipeg%2C%20MB&dc=true&ll=49.8954221%2C-97.1385145&radius=16.0&view=list'
+KIJIJI_SEARCH_URL = 'https://www.kijiji.ca/b-winnipeg/laptop/k0l1700192?address=Winnipeg%2C%20MB&dc=true&ll=49.8954221%2C-97.1385145&radius=16.0&view=list'
 
 CHECK_INTERVAL = 300
